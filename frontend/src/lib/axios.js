9f433f5b471dc68e7a619555a717e3b9f81caecb
Mamoudou Thiam <doums.job@gmail.com>
2025-08-01 18:01:07 +0200

    prepared for the deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-// n production, il n'y a pas de localhost donc nous devons le rendre dynamique
+// En production, il n'y a pas de localhost donc nous devons le rendre dynamique
 
-const BASE_URL = import.meta.nv.MODE === "development" ? "http://localhost:5001/api" : "/api"
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api"
 const api = axios.create({
     baseURL : BASE_URL,
 });
