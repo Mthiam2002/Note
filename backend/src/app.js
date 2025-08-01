acfa873c1d33b16f398c2bf484b90deb2b669596
Mamoudou Thiam <doums.job@gmail.com>
2025-08-01 16:58:06 +0200

    prepared for the deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import path from "path";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
 
@@ -11,13 +12,18 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 
 
 //middleware
-app.use(cors({
-    origin: "http://localhost:5173",
-}));
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin: "http://localhost:5173",
+        })
+    );
+}
+
 
 app.use(express.json());
 
@@ -35,6 +41,14 @@ app.use((req, res, next) => {
 */
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req,res) => {
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+    });
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+// n production, il n'y a pas de localhost donc nous devons le rendre dynamique
+
+const BASE_URL = import.meta.nv.MODE === "development" ? "http://localhost:5001/api" : "/api"
 const api = axios.create({
-    baseURL : "http://localhost:5001/api"
+    baseURL : BASE_URL,
 });
 
 export default api;
